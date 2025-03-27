5701159b7fca569a93512899f75c2e0f63d37b84
louie <louiehansy@gmail.com>
2025-03-27 13:53:14 +0800

    corrected unique restriction

--- library_extensions/models/library_book_category.py
+++ library_extensions/models/library_book_category.py
@@ -3,11 +3,11 @@ from odoo import models, fields
 class LibraryBookCategory(models.Model):
     _name = "library.book.category"
     _description = "Book Category"
+    _sql_constraints = [('unique_name', 'UNIQUE(name)', 'Name must be unique!')]
 
     name = fields.Char(
         string="Category Name",
-        required=True,
-        unique=True
+        required=True
     )
 
     
